69d356390e1fa886f5f1ab85c9d40881ebe8fbef
BlankaKorvo <BlankaKorvo@gmail.com>
2022-02-04 12:56:40 +0300

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,36 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.Versioning;
 using System.Threading.Tasks;
 using Serilog;
-
 using DataCollector;
-using CandleInterval = MarketDataModules.Candles.CandleInterval;
-using MarketDataModules;
 
 using System.Linq;
 using System.IO;
 
 using MarketDataModules.Candles;
 
-using Instrument = MarketDataModules.Instruments.Instrument;
-using LinqStatistics;
-
-
-using System.Threading;
-using MarketDataModules.Portfolio;
-using MarketDataModules.Operation;
-using System.Reflection;
-//using Analysis.Screeners.Helpers;
-using Analysis.Screeners.StockExchangeDataScreener;
-using Analysis.Signals;
-using Analysis.TradeDecision;
-using MarketDataModules.Orderbooks;
-using DataCollector.TinkoffAdapter;
 using System.Diagnostics;
-using MarketDataModules.Trading;
-using Research;
 
+using Research;
+using Skender.Stock.Indicators;
+using Analysis;
 
 namespace tradeSDK
 {
@@ -44,13 +27,19 @@ namespace tradeSDK
                 .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs//.log"), rollingInterval: RollingInterval.Month, fileSizeLimitBytes: 304857600, rollOnFileSizeLimit: true)
                 .CreateLogger();
 
+
+
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            string ticker = "Amzn";
+            string ticker = "AMZN";
             var instrument = await GetMarketData.GetInstrumentByTickerAsync(ticker);
             ICandlesList candlesList = await GetMarketData.GetCandlesAsync(instrument.Figi, CandleInterval.FiveMinutes, DateTime.Now.AddDays(-365));
+            stopwatch.Start();
+            //List<ParabolicSarResult> parabolicSarTrand = MapperCandlesToQuote.ConvertThisCandlesToQuote(candlesList.Candles).GetParabolicSar(accelerationStep, maxAccelerationFactor, initialFactor).ToList();
             new OfflineResearch(candlesList).Research();
             stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
 
 
             Console.ReadKey();
