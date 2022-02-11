debc1e961f9e31c3b4c7c00b1567c12477927200
BlankaKorvo <BlankaKorvo@gmail.com>
2022-02-11 18:00:53 +0300

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -25,16 +25,16 @@ namespace tradeSDK
             stopwatch.Start();
             string ticker = "NVR";
             var instrument = await GetMarketData.GetInstrumentByTickerAsync(ticker);
-            ICandlesList candlesList = await GetMarketData.GetCandlesAsync(instrument.Figi, CandleInterval.Minute, DateTime.Now.AddDays(-380));
+            ICandlesList candlesList = await GetMarketData.GetCandlesAsync(instrument.Figi, CandleInterval.FiveMinutes, DateTime.Now.AddDays(-380));
             stopwatch.Start();
             //List<ParabolicSarResult> parabolicSarTrand = MapperCandlesToQuote.ConvertThisCandlesToQuote(candlesList.Candles).GetParabolicSar(accelerationStep, maxAccelerationFactor, initialFactor).ToList();
             new OfflineResearch(candlesList).Research();
             stopwatch.Stop();
-            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            //Console.WriteLine(stopwatch.ElapsedMilliseconds);
 
 
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 
