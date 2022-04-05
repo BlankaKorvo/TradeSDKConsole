65f9064d29d0875e12a74d0b80b411173124fbde
BlankaKorvo <BlankaKorvo@gmail.com>
2022-04-05 11:32:18 +0300

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -15,19 +15,20 @@ namespace tradeSDK
         {
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
-                //.WriteTo.Console()
+                .WriteTo.Console()
                 .WriteTo.File(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs//.log"), rollingInterval: RollingInterval.Month, fileSizeLimitBytes: 304857600, rollOnFileSizeLimit: true)
                 .CreateLogger();
 
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            string ticker = "AAPL";
+            string ticker = "Yndx";
             var instrument = await GetMarketData.GetInstrumentByTickerAsync(ticker);
 
             stopwatch.Start();
             //List<ParabolicSarResult> parabolicSarTrand = MapperCandlesToQuote.ConvertThisCandlesToQuote(candlesList.Candles).GetParabolicSar(accelerationStep, maxAccelerationFactor, initialFactor).ToList();
-            TimeSpan timeSpan = TimeSpan.FromDays(360);
-            await new OfflineResearch(instrument.Figi,CandleInterval.Minute, timeSpan, 400).Start();
+            TimeSpan timeSpan = TimeSpan.FromDays(2);
+            //await new OfflineResearch(instrument.Figi,CandleInterval.Minute, timeSpan, 400).Start();
+            await new OnlineResearch(instrument.Figi, CandleInterval.Minute, 200).Start();
             stopwatch.Stop();
             Console.WriteLine(stopwatch.ElapsedMilliseconds);
             Console.ReadKey();
