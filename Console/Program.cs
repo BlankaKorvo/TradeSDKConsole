7603e682dd56fd9ceb81f5664c9c1ce778a42e8b
BlankaKorvo <BlankaKorvo@gmail.com>
2022-04-29 17:03:28 +0300

    end day

--- Console/Program.cs
+++ Console/Program.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using ResearchLib;
 using Analysis.TradeDecision;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace tradeSDK
 {
@@ -27,19 +28,35 @@ namespace tradeSDK
             var instrument = await GetMarketData.GetInstrumentByTickerAsync(ticker);
             //Decision decision1 = TradeDecisions.Method1;
             List<Decision> decisions = new List<Decision> { 
-                TradeDecisions.BBBorder10_02, 
-                TradeDecisions.BBBorder10_02DeleteOrder, 
-                TradeDecisions.BBBorder20_02,
-                TradeDecisions.BBBorder20_02DeleteOrder,
-                TradeDecisions.BBPercentOverCross1040,
-                TradeDecisions.BBPercentOverCross1040DeleteOrder,
-                TradeDecisions.BBPercentOverCross2010,
-                TradeDecisions.BBPercentOverCross2010DeleteOrder,
-                TradeDecisions.BBPercentPeriodCross1020,
-                TradeDecisions.BBPercentPeriodCross1020DeleteOrder,
-                TradeDecisions.BBPercentPeriodCross1030,
-                TradeDecisions.BBPercentPeriodCross1030DeleteOrder
+                //TradeDecisions.BBBorder10_02, 
+                //TradeDecisions.BBBorder10_02DeleteOrder, 
+                //TradeDecisions.BBBorder20_02,
+                //TradeDecisions.BBBorder20_02DeleteOrder,
+                //TradeDecisions.BBPercentOverCross1040,
+                //TradeDecisions.BBPercentOverCross1040DeleteOrder,
+                //TradeDecisions.BBPercentOverCross1060,
+                //TradeDecisions.BBPercentOverCross1060DeleteOrder,
+                //TradeDecisions.BBPercentOverCross2010,
+                //TradeDecisions.BBPercentOverCross2010DeleteOrder,
+                //TradeDecisions.BBPercentPeriodCross1020,
+                //TradeDecisions.BBPercentPeriodCross1020DeleteOrder,
+                //TradeDecisions.BBPercentPeriodCross1030,
+                //TradeDecisions.BBPercentPeriodCross1030DeleteOrder,
+                //TradeDecisions.BBPercentOverCross2040,
+                //TradeDecisions.BBPercentOverCross2040DeleteOrder,
+                //TradeDecisions.BBPercentOverCross2060,
+                //TradeDecisions.BBPercentOverCross2060DeleteOrder                
             };
+
+            var x = typeof(TradeDecisions).GetMethods();
+            foreach (var item in x.Where(x => x.IsStatic))
+            {
+                if (item.DeclaringType == typeof(TradeDecisions))
+                {
+                    decisions.Add(item.CreateDelegate<Decision>());
+                }
+            }
+
             OnlineResearch onlineResearch = new(instrument.Figi, CandleInterval.FiveMinutes, 400);
             try
             {
