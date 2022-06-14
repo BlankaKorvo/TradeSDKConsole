0bae353f26cca334749edd0720e88eda6bf377fb
BlankaKorvo <BlankaKorvo@gmail.com>
2022-06-14 18:36:48 +0300

    some fix

--- Console/Program.cs
+++ Console/Program.cs
@@ -33,24 +33,7 @@ namespace tradeSDK
             var instrument = await GetMarketData.GetInstrumentByTickerAsync(ticker);
             //Decision decision1 = TradeDecisions.Method1;
             List<Decision> decisions = new List<Decision> { 
-                //TradeDecisions.BBBorder10_02, 
-                //TradeDecisions.BBBorder10_02DeleteOrder, 
-                //TradeDecisions.BBBorder20_02,
-                //TradeDecisions.BBBorder20_02DeleteOrder,
-                //TradeDecisions.BBPercentOverCross1040,
-                //TradeDecisions.BBPercentOverCross1040DeleteOrder,
-                //TradeDecisions.BBPercentOverCross1060,
-                //TradeDecisions.BBPercentOverCross1060DeleteOrder,
-                //TradeDecisions.BBPercentOverCross2010,
-                //TradeDecisions.BBPercentOverCross2010DeleteOrder,
-                //TradeDecisions.BBPercentPeriodCross1020,
-                //TradeDecisions.BBPercentPeriodCross1020DeleteOrder,
-                //TradeDecisions.BBPercentPeriodCross1030,
-                //TradeDecisions.BBPercentPeriodCross1030DeleteOrder,
-                //TradeDecisions.BBPercentOverCross2040,
-                //TradeDecisions.BBPercentOverCross2040DeleteOrder,
-                //TradeDecisions.BBPercentOverCross2060,
-                //TradeDecisions.BBPercentOverCross2060DeleteOrder                
+            
             };
 
             var x = typeof(TradeDecisions).GetMethods();
