724fa2d4d873bad2f7fa02be374f01d0a831c4e1
BlankaKorvo <BlankaKorvo@gmail.com>
2022-08-10 11:19:37 +0300

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -27,34 +27,41 @@ namespace tradeSDK
                               )
                 .CreateLogger();
 
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            string ticker = "AMZN";
+            int x = 1;
+            string ticker = "YNDX";
             var instrument = await GetMarketData.GetInstrumentByTickerAsync(ticker);
-            //Decision decision1 = TradeDecisions.Method1;
-            List<Decision> decisions = new List<Decision> { 
-            
-            };
-
-            var x = typeof(TradeDecisions).GetMethods();
-            foreach (var item in x.Where(x => x.IsStatic))
+            CandlesList candlesList = await GetMarketData.GetCandlesAsync(instrument.Figi, CandleInterval.Day, 1000);
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.csv"), true, System.Text.Encoding.Default))
             {
-                if (item.DeclaringType == typeof(TradeDecisions))
+                foreach (var candle in candlesList.Candles)
                 {
-                    decisions.Add(item.CreateDelegate<Decision>());
+                        sw.WriteLine($"{x++};{candle.Close}");                
                 }
             }
+            ////Decision decision1 = TradeDecisions.Method1;
+            //List<Decision> decisions = new List<Decision> { 
 
-            OnlineResearch onlineResearch = new(instrument.Figi, CandleInterval.FiveMinutes, 400);
-            try
-            {
-                await onlineResearch.Start(decisions);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex.Message);
-                Log.Error(ex.StackTrace);
-            }
+            //};
+
+            //var x = typeof(TradeDecisions).GetMethods();
+            //foreach (var item in x.Where(x => x.IsStatic))
+            //{
+            //    if (item.DeclaringType == typeof(TradeDecisions))
+            //    {
+            //        decisions.Add(item.CreateDelegate<Decision>());
+            //    }
+            //}
+
+            //OnlineResearch onlineResearch = new(instrument.Figi, CandleInterval.FiveMinutes, 400);
+            //try
+            //{
+            //    await onlineResearch.Start(decisions);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Log.Error(ex.Message);
+            //    Log.Error(ex.StackTrace);
+            //}
         }
     }
 }
